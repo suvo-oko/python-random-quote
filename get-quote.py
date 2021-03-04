acf3c6882c70683f365ed726bfec4f2d1be79590
suvo-oko <miravalles@gmail.com>
2021-03-04 10:48:52 +0100

    Updated get-quote.py to fetch 2 random quotes

--- get-quote.py
+++ get-quote.py
@@ -1,15 +1,14 @@
 import random
 
 def primary():
-  # print("Keep it logically awesome.")
-
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  last = 16
-  rnd = random.randint(0, last)
-  print(quotes[rnd])
+  for i in range(0,2):
+        last=16
+        rnd=random.randint(0,last)
+        print(quotes[rnd])
 
 if __name__== "__main__":
   primary()
