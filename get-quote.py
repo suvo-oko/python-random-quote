f4ff1e4ac408378855e47a549c546e0474a8641b
suvo-oko <miravalles@gmail.com>
2021-03-10 07:35:43 +0100

    Update get-quote.py to add new line

--- get-quote.py
+++ get-quote.py
@@ -1,14 +1,16 @@
-import random
+import random  # a module to generate random numbers
 
 def primary():
+  f = open("quotes.txt", "a")
+  f.write("Be radically transparent\n")
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
   for i in range(0,2):
-        last=16
+        last=17
         rnd=random.randint(0,last)
         print(quotes[rnd].rstrip())
 
 if __name__== "__main__":
-  primary()
+    primary()
